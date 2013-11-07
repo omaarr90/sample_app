228d0ad016ea3f8659fe3e4e108f9ae1daec3c6d
Omar Alshammari <omar.alshammary@hotmail.com>
2013-11-08 02:51:45 +0300

    Fix a bug when user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,12 +34,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
